41730b220d5932b2ae229dc462d2bc6b64fa6d4d
Alexander Obregon <alexander.obregon47@gmail.com>
2023-01-26 16:22:33 -0800

    added imports for GreetingController

--- rest-service1/src/main/java/com/example/restservice1/GreetingController.java
+++ rest-service1/src/main/java/com/example/restservice1/GreetingController.java
@@ -1,5 +1,12 @@
 package com.example.restservice1;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
 public class GreetingController {
     
 }
