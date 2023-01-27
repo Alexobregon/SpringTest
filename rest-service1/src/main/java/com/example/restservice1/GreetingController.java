a04d178fced66c83f7402177768795c69972e1c7
Alexander Obregon <alexander.obregon47@gmail.com>
2023-01-26 16:23:39 -0800

    added mapping request

--- rest-service1/src/main/java/com/example/restservice1/GreetingController.java
+++ rest-service1/src/main/java/com/example/restservice1/GreetingController.java
@@ -8,5 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value ="name", defaultValue = "World") String name) {
+		
+		return null;
+	}
     
 }
